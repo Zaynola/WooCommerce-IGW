0a317701b627e8148fa3e904e2b2a5d803e7dfd0
Olaolu Odushola <olaoluodushola@gmail.com>
2024-02-10 14:43:12 -0500

    Initial commit

---
+++ test/bmo-woo_commerce-igw.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BmoWooCommerceIgw from '../lib/bmo-woo_commerce-igw-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bmo-woo_commerce-igw-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BmoWooCommerceIgw.BmoWooCommerceIgwStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
